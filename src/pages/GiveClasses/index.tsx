94efbd1b6299b71b8acbe8de106b556962314606
Glauber Brack <glauber@brack.com.br>
2020-08-11 22:31:55 -0300

    finishing give classes page

--- src/pages/GiveClasses/index.tsx
+++ src/pages/GiveClasses/index.tsx
@@ -1,10 +1,37 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ImageBackground, Text } from 'react-native';
+
+import backgroundImg from '../../assets/images/give-classes-background.png';
 
 import styles from './styles';
+import { RectButton } from 'react-native-gesture-handler';
+import { useNavigation } from '@react-navigation/native';
 
 const GiveClasses = () => {
-    return <View/>
+    const { goBack } = useNavigation();
+
+    function handleNavigateBack() {
+        goBack();
+    }
+
+    return (
+        <View style={styles.container}>
+            <ImageBackground resizeMode="contain" source={backgroundImg} style={styles.content}>
+                <Text style={styles.title}>
+                    Want to be a Proffy?
+                </Text>
+                <Text style={styles.description}>
+                    First of all, you need to create an account on our web plataform.
+                </Text>
+            </ImageBackground>
+            
+            <RectButton onPress={handleNavigateBack} style={styles.okButton}>
+                <Text style={styles.okButtonText}>
+                    Ok then :(
+                </Text>
+            </RectButton>
+        </View>
+    );
 }
 
 export default GiveClasses;
--- src/pages/GiveClasses/styles.ts
+++ src/pages/GiveClasses/styles.ts
@@ -1,7 +1,51 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-    
+    container: {
+        flex: 1,
+        backgroundColor: '#082A44',
+        justifyContent: 'center',
+        padding: 40
+    },
+
+    content: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+
+    title: {
+        fontFamily: 'Archivo_700Bold',
+        color: '#fff',
+        fontSize: 32,
+        lineHeight: 37,
+        maxWidth: 180
+    },
+
+    description: {
+        marginTop: 24,
+        color: '#FAFAFC',
+        fontSize: 16,
+        lineHeight: 26,
+        fontFamily: 'Poppins_400Regular',
+        maxWidth: 240
+    },
+
+    okButton: {
+        marginVertical: 40,
+        backgroundColor: '#22a889',
+        height: 58,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 8
+    },
+
+    okButtonText: {
+        color: '#fff',
+        fontFamily: 'Archivo_700Bold',
+        fontSize: 16
+
+    }
+
 });
 
 export default styles;
