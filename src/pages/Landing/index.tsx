a8f4414365cb3bd0522af763548a914975a2d90e
Glauber Brack <glauber@brack.com.br>
2020-08-11 22:01:58 -0300

    finishing landing page

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
-import { View, Image, Text } from 'react-native';
+import { View, Image, Text, TouchableOpacity } from 'react-native';
 
 import styles from './styles';
 
 import landingImage from '../../assets/images/landing.png';
+import studyIcon from '../../assets/images/icons/study.png';
+import giveClassesIcon from '../../assets/images/icons/give-classes.png';
+import heartIcon from '../../assets/images/icons/heart.png';
 
 const Landing = () => {
     return(
@@ -15,6 +18,23 @@ const Landing = () => {
                 <Text style={styles.titleBold}>
                     What do you want to do?
                 </Text>
+            </Text>
+
+            <View style={styles.buttonsContainer}>
+                <TouchableOpacity style={[styles.button, styles.buttonPrimary]}>
+                    <Image source={studyIcon}/>
+                    <Text style={styles.buttonText}>Study</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={[styles.button, styles.buttonSecondary]}>
+                    <Image source={giveClassesIcon}/>
+                    <Text style={styles.buttonText}>Give Classes</Text>
+                </TouchableOpacity>
+            </View>
+
+            <Text style={styles.totalConnections}>
+                100 connections already made {' '}
+                <Image source={heartIcon}/>
             </Text>
         </View>
     );
--- src/pages/Landing/styles.ts
+++ src/pages/Landing/styles.ts
@@ -3,7 +3,7 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#8257e5',
+        backgroundColor: '#082A44',
         justifyContent: 'center',
         padding: 40
     },
@@ -14,14 +14,52 @@ const styles = StyleSheet.create({
     },
 
     title: {
-        color: '#FFF',
+        fontFamily: 'Poppins_400Regular',
+        color: '#fff',
         fontSize: 20,
         lineHeight: 30,
         marginTop: 80
     },
 
     titleBold: {
-        fontWeight: 'bold'
+        fontFamily: 'Poppins_600SemiBold'
+    },
+
+    buttonsContainer: {
+        flexDirection: 'row',
+        marginTop: 40,
+        justifyContent: 'space-between'
+    },
+
+    button: {
+        height: 150,
+        width: '48%',
+        borderRadius: 8,
+        padding: 24,
+        justifyContent: 'space-between',
+    },
+
+    buttonPrimary: {
+        backgroundColor: '#41BCFF',
+    },
+
+    buttonSecondary: {
+        backgroundColor: '#2BD2AA'
+    },
+
+    buttonText: {
+        fontFamily: 'Archivo_700Bold',
+        color: '#fff',
+        fontSize: 20
+    },
+
+    totalConnections: {
+        fontFamily: 'Poppins_400Regular',
+        color: '#fff',
+        fontSize: 12,
+        lineHeight: 20,
+        maxWidth: 240,
+        marginTop: 40
     }
 
 })
