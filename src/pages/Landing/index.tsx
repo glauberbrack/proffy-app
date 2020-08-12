89dcfa3bec715c51cde1362f0b81bac5f6a68de0
Glauber Brack <glauber@brack.com.br>
2020-08-11 21:44:46 -0300

    importing external fonts

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,8 +1,23 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Image, Text } from 'react-native';
+
+import styles from './styles';
+
+import landingImage from '../../assets/images/landing.png';
 
 const Landing = () => {
-    return <View/>
+    return(
+        <View style={styles.container}>
+            <Image source={landingImage} style={styles.banner}/>
+
+            <Text style={styles.title}>
+                Hi, there! {'\n'}
+                <Text style={styles.titleBold}>
+                    What do you want to do?
+                </Text>
+            </Text>
+        </View>
+    );
 }
 
 export default Landing;
